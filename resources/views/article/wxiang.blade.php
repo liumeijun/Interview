b4dd421c7ece07f727dd05ebc45460a13768beab
Your Name <you@example.com>
2016-08-16 09:57:01 +0800

    时庆庆的用户头像-一周雷锋榜

--- resources/views/article/wxiang.blade.php
+++ resources/views/article/wxiang.blade.php
@@ -94,7 +94,29 @@ var isLogin=1
                         <div class="bdsharebuttonbox weichat-style">
                             <a href="#" class="bds_weixin icon-nav icon-share-weichat" data-cmd="weixin" title="分享到微信"></a>
                             <a href="#" class="bds_tsina icon-nav icon-share-weibo" data-cmd="tsina" title="分享到新浪微博"></a>
-                            <a href="" class="bds_qzone icon-nav icon-share-qq" data-cmd="qzone" title="分享到QQ空间"></a>
+                            {{--<a href="" class="bds_qzone icon-nav icon-share-qq" data-cmd="qzone" title="分享到QQ空间"></a>--}}
+                            <script type="text/javascript">
+                                (function(){
+                                    var p = {
+                                        url:location.href,
+                                        showcount:'0',/*是否显示分享总数,显示：'1'，不显示：'0' */
+                                        desc:'',/*默认分享理由(可选)*/
+                                        summary:'',/*分享摘要(可选)*/
+                                        title:'',/*分享标题(可选)*/
+                                        site:'',/*分享来源 如：腾讯网(可选)*/
+                                        pics:'', /*分享图片的路径(可选)*/
+                                        style:'203',
+                                        width:98,
+                                        height:22
+                                    };
+                                    var s = [];
+                                    for(var i in p){
+                                        s.push(i + '=' + encodeURIComponent(p[i]||''));
+                                    }
+                                    document.write(['<a version="1.0"  class="bds_qzone icon-nav icon-share-qq" title="分享到QQ空间" href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?',s.join('&'),'" target="_blank"></a>'].join(''));
+                                })();
+                            </script>
+                            <script src="http://qzonestyle.gtimg.cn/qzone/app/qzlike/qzopensl.js#jsdate=20111201" charset="utf-8"></script>
                         </div>
                     </li>
                 </ul>
